5210486c901f8a5848a66ca989596a5829b9352e
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-09-28 19:20:13 +0000

    Upgrade to android-6.0.0_r7

--- get-sources.sh
+++ get-sources.sh
@@ -9,7 +9,7 @@ tar -xvzf openssl-1.0.2j.tar.gz
 # -------------------------
 
 # Branch to checkout from Android source code repo
-branch=android-5.1.1_r38
+branch=android-6.0.0_r7
 
 # DOWNLOAD necessary files
 # -------------------------
