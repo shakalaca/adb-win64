d311777408582d7222863003e71cfb9369852978
vagrant <vagrant@vagrant.vm>
2016-09-28 17:04:49 +0000

    Build adb from Android 4.4 for x64 Windows, without USB support

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Makefile to use (will be automatically copied into system/core/adb)
+makefile=makefile.sample
+
+
+# MAKE
+# -------------------------
+echo "\n>> Copying makefile into system/core/adb...\n"
+cp $makefile android-adb/system/core/adb/makefile -f
+cd android-adb/system/core/adb/
+echo "\n>> Make... \n"
+make clean
+make
+echo "\n>> Copying adb back into current dir...\n"
+cp adb ../../../../
+echo "\n>> FINISH!\n"
---
+++ get-sources.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#         DEPENDENCIES
+# -------------------------
+wget https://www.openssl.org/source/openssl-1.0.2j.tar.gz
+tar -xvzf openssl-1.0.2j.tar.gz
+
+#         CONFIG
+# -------------------------
+
+# Branch to checkout from Android source code repo
+branch=android-4.4.4_r2.0.1
+
+# DOWNLOAD necessary files
+# -------------------------
+echo "\n>> >>> ADB for ARM <<< \n"
+echo "\n>> Downloading necessay files ($branch branch)\n"
+mkdir android-adb
+cd android-adb
+mkdir system
+cd system
+git clone -b $branch https://android.googlesource.com/platform/system/core
+git clone -b $branch https://android.googlesource.com/platform/system/extras
+cd ..
+mkdir external
+cd external
+git clone -b $branch https://android.googlesource.com/platform/external/zlib
+git clone -b $branch https://android.googlesource.com/platform/external/openssl
+git clone -b $branch https://android.googlesource.com/platform/external/libselinux
+cd ..
+mkdir platform
+git clone -b $branch https://android.googlesource.com/platform/development/
